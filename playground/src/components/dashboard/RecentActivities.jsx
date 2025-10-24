9e2b23ded8d232eea771f6ffbb661910b4a33c5c
htkael <htkael@gmail.com>
2025-10-24 13:26:15 -0500

    remove logs

--- playground/src/components/dashboard/RecentActivities.jsx
+++ playground/src/components/dashboard/RecentActivities.jsx
@@ -23,8 +23,6 @@ export const RecentActivities = ({ recentActivities }) => {
     }).filter(Boolean)
   }
 
-  console.log("recentActivities", recentActivities)
-
   return (
     <div className="card bg-base-100 shadow-sm">
       <div className="card-body">
